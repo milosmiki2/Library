efa3c80ac610a4f167cf770068d7e0cbcfcd2a21
Milos Manojlovic <malidzidza@gmail.com>
2025-06-30 02:24:08 +0200

    Hello!!!

--- script.js
+++ script.js
@@ -17,21 +17,83 @@ const bookRead = document.querySelector("#book-read");
 const newBook = document.querySelector(".new-book-button");
 const addBook = document.querySelector(".add-book-button");
 const form = document.querySelector("form");
+const input = document.querySelector("input");
+const main = document.querySelector(".main");
+
+newBook.addEventListener("click", popup);
+addBook.addEventListener("click", addBookToLibrary);
+
+function popup() {
+  form.style.display = "flex";
+  // body.style.filter = "blur(4px)";
+
+  // grid.classList.add("blur");
+  // body.addEventListener("click", (e) => {
+  //   if (e.target !== newBook) {
+  //     form.style.display = "none";
+  //     form.reset();
+  //   }
+  // });
+}
 
 function addBookToLibrary(event) {
   if (form.checkValidity() === true) {
     event.preventDefault();
     crypto.randomUUID();
+
     let title = bookTitle.value;
     let author = bookAuthor.value;
     let pages = bookPages.value;
     let read;
     if (bookRead.checked) {
-      read = "Read";
-    } else read = "Not read";
+      read = "true";
+    } else read = "false";
     myLibrary.push(new Book(title, author, pages, read));
-    console.log(myLibrary);
+
+    const card = document.createElement("div");
+    const cardAuthor = document.createElement("p");
+    const cardTitle = document.createElement("p");
+    const cardPages = document.createElement("p");
+    const cardRead = document.createElement("button");
+    const cardRemove = document.createElement("button");
+    card.classList.add("card");
+    cardRead.classList.add("read");
+    cardRemove.classList.add("remove");
+    cardRemove.textContent = "Remove";
+    main.appendChild(card);
+    card.appendChild(cardTitle);
+    card.appendChild(cardAuthor);
+    card.appendChild(cardPages);
+    card.appendChild(cardRead);
+    card.appendChild(cardRemove);
+
+    cardRemove.addEventListener("click", () => {
+      card.remove();
+    });
+
+    cardRead.addEventListener("click", () => {
+      cardRead.classList.toggle("not-read");
+      if (cardRead.textContent === "Not Read") {
+        cardRead.textContent = "Read";
+        book.read = "true";
+      } else cardRead.textContent = "Not Read";
+      book.read = "false";
+    });
+
+    myLibrary.forEach((book) => {
+      let uuid = crypto.randomUUID();
+      card.style.display = "flex";
+      cardTitle.textContent = '" ' + book.title + ' "';
+      cardAuthor.textContent = book.author;
+      cardPages.textContent = book.pages;
+      if (book.read === "false") {
+        cardRead.classList.add("not-read");
+        cardRead.textContent = "Not Read";
+      } else {
+        cardRead.classList.remove("not-read");
+        cardRead.textContent = "Read";
+      }
+    });
+    form.reset();
   }
 }
-
-addBook.addEventListener("click", addBookToLibrary);
